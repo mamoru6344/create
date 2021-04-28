385ca4d69a18f524f51f2928b8c11cd07666ca3e
mamoru6344 <mamoru6344@gmail.com>
2021-04-28 23:17:22 +0900

    【更新】 Bustabit アプリ化 ログイン機能

---
+++ バスタビット/スクレイピング/内部/login.py
@@ -0,0 +1,29 @@
+import time
+import chromedriver_binary 
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException
+import os
+import signal
+
+#固定値宣言
+USER_PROFILE = 'UserProfile'
+
+def login():
+  try:
+    #テスト用
+    driver = webdriver.Chrome('D:\プログラミング\chromedriver_win32\chromedriver')
+
+    # Optionでユーザプロファイルの場所を指定する
+    options = webdriver.ChromeOptions()
+    options.add_argument('--user-data-dir=' + USER_PROFILE)
+
+    #URL取得
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://www.bustabit.com/play')
+
+  finally:
+    os.kill(driver.service.process.pid,signal.SIGTERM)
+
+if __name__ == "__main__":
+  login()
